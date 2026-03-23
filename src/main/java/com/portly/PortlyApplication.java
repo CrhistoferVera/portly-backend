8cc3594279ee03b81443417a84a04f9a9c05ab6c
CrhistoferVera <cristofervera110@gmail.com>
2026-03-22 22:40:04 -0400

    Initialize project with Maven setup, including wrapper scripts, POM file, and application properties

---
+++ src/main/java/com/portly/PortlyApplication.java
@@ -0,0 +1,13 @@
+package com.portly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portly/PortlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
